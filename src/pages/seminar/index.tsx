5b6766a4ae29d14915029452a81136d751b08006
ericka.bermudez <37844174+ErickaBermudez@users.noreply.github.com>
2023-05-30 02:19:02 +0900

    feat: 2 more pages

---
+++ src/pages/seminar/index.tsx
@@ -0,0 +1,11 @@
+import { Layout } from "@/components/templates"
+
+const Seminar = () => {
+  return (
+    <Layout>
+      <h1>Paper Discussion</h1>
+    </Layout>
+  )
+}
+
+export default Seminar
